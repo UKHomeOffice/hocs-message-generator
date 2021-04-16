2e05f1c6490f11c26b6a2420767e6e626bcfd627
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-04-16 15:06:22 +0100

    HOCS-2659 : Default jar name points to dockerfile variable

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-NAME=${NAME:-case-creator}
+NAME=${NAME:-SHOULD_BE_SET_IN_DOCKER_FILE}
 
 JAR=$(find . -name ${NAME}*.jar|head -1)
 java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
